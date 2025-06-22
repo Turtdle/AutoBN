462c37c54183bfe5f944c17cf0a3e07d27fd90b1
Turtdle <root@Zephyrus14inch.localdomain>
2025-06-22 00:00:42 -0700

    a

--- autobn.py
+++ autobn.py
@@ -422,7 +422,7 @@ if __name__ == "__main__":
             time.sleep(0.5)
             pyautogui.click(1570,1031)
             pyautogui.screenshot("shared_folder/screenshot.png")
-            time.sleep(0.5)
+            time.sleep(1.5)
             pyautogui.click(1570,1031)
             counter+=1
             
